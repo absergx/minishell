c66d19eb523a2293ffdde86febe6e54684b85ade
Meldred Emilio <memilio@er-e2.kzn.21-school.ru>
2020-09-21 20:27:38 +0300

    backslashes

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,54 @@
 /*   By: memilio <memilio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 17:22:15 by memilio           #+#    #+#             */
-/*   Updated: 2020/09/17 19:22:43 by memilio          ###   ########.fr       */
+/*   Updated: 2020/09/21 20:27:15 by memilio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include "minishell.h"
+
+char	*get_line(void);
+
+int		last_backslashes(char *line)
+{
+	size_t	len;
+	int		count;
+
+	count = 0;
+	len = ft_strlen(line);
+	while (len >= 0 && line[len - 1] == '\\')
+	{
+		++count;
+		--len;
+	}
+	return (count % 2);
+}
+
+char	*check_backslash(char *line)
+{
+	
+	char	*tmp;
+	char	*ret;
+
+	tmp = NULL;
+	ret = NULL;
+	if (last_backslashes(line))
+	{
+		line[ft_strlen(line) - 1] = '\0';
+		ft_putstr_fd("\\ ", 0);
+		tmp = get_line();
+		ret = ft_strjoin(line, tmp);
+		free(line);
+		free(tmp);
+		if (last_backslashes(ret))
+			return (check_backslash(ret));
+	}
+	else
+		return (line);
+	return (ret);
+}
 
 char	*get_line(void)
 {
@@ -21,7 +62,7 @@ char	*get_line(void)
 	line = NULL;
 	if (get_next_line(0, &line) < 0)
 		ft_putstr_fd("Error\n", 0);
-	return (line);
+	return (check_backslash(line));
 }
 
 int		main(void)
